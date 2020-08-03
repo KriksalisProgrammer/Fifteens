3f18b4c5577c79975077f1133a1c46e388b9c599
Антон Воробйов <anton.vorobiov@nure.ua>
2020-08-03 13:02:51 +0300

    Добавьте файлы проекта.

---
+++ Fifteen/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Fifteen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.StartGame = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Сounter = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Restart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(271, 270);
+            this.panel1.TabIndex = 0;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(90, 180);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(90, 90);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(0, 180);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(90, 90);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(180, 90);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 90);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(90, 90);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 90);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 90);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 90);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(180, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 90);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(90, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 90);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 90);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StartGame
+            // 
+            this.StartGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.StartGame.Location = new System.Drawing.Point(58, 332);
+            this.StartGame.Name = "StartGame";
+            this.StartGame.Size = new System.Drawing.Size(150, 60);
+            this.StartGame.TabIndex = 1;
+            this.StartGame.Text = "StartGame";
+            this.StartGame.UseVisualStyleBackColor = true;
+            this.StartGame.Click += new System.EventHandler(this.StartGame_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Fifteen.Properties.Resources.Image;
+            this.pictureBox1.Location = new System.Drawing.Point(308, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(391, 270);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Сounter
+            // 
+            this.Сounter.AutoSize = true;
+            this.Сounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Сounter.Location = new System.Drawing.Point(30, 350);
+            this.Сounter.Name = "Сounter";
+            this.Сounter.Size = new System.Drawing.Size(225, 26);
+            this.Сounter.TabIndex = 3;
+            this.Сounter.Text = "Количество шагов: 0";
+            // 
+            // Exit
+            // 
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Exit.Location = new System.Drawing.Point(526, 332);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(150, 60);
+            this.Exit.TabIndex = 4;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Restart
+            // 
+            this.Restart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Restart.Location = new System.Drawing.Point(285, 332);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(150, 60);
+            this.Restart.TabIndex = 5;
+            this.Restart.Text = "Restart";
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // label1
+            // 
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label1.Location = new System.Drawing.Point(91, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(567, 129);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Нажмите StartGame, что бы начать игру)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 404);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartGame);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Сounter);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Пятнашки";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button StartGame;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Сounter;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Restart;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Fifteen/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Fifteen
+{
+    public partial class Form1 : Form
+    {
+        Point Empty;
+        ArrayList image = new ArrayList();
+        
+        int Counters;
+        public Form1()
+        {
+            Empty.X = 180;
+            Empty.Y = 180;
+            InitializeComponent();
+        }
+
+       
+
+        private void AddImage(ArrayList images)
+        {
+            int i = 0;
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7 }; 
+            arr = Treatment(arr);
+            foreach(Button b in panel1.Controls)
+            {
+                if(i<arr.Length)
+                {
+                    b.Image =(Image)image[arr[i]];
+                    i++;
+                }
+            }
+        }
+        private int[] Treatment(int[]arr)
+        {
+            var rand = new Random();
+            arr = arr.OrderBy(x => rand.Next()).ToArray();
+            return arr;
+        }
+
+        private void Crop(Image or, int x, int y)
+        {
+            Bitmap bitmap = new Bitmap(x, y);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.DrawImage(or, 0, 0, x,y);
+            graphics.Dispose();
+            int move = 0, movd = 0;
+            for(int i=0;i<8;i++)
+            {
+                Bitmap p = new Bitmap(90, 90);
+                for(int j=0;j<90;j++)
+                {
+                    for(int k=0;k<90;k++)
+                    {
+                        p.SetPixel(j, k, bitmap.GetPixel(j + move, k + movd));
+                    }
+                }
+                image.Add(p);
+                move += 90;
+                if(move==270)
+                {
+                    move = 0;
+                    movd += 90;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MoveButton((Button)sender);
+           
+        }
+
+        private void MoveButton(Button btn)
+        {
+            if(((btn.Location.X==Empty.X-90||btn.Location.X==Empty.X+90)&& btn.Location.Y == Empty.Y)
+                || (btn.Location.Y == Empty.Y - 90 || btn.Location.Y == Empty.Y + 90) && btn.Location.X == Empty.X)
+            {
+                Point swap = btn.Location;
+                btn.Location = Empty;
+                Empty = swap;
+                Counters++;
+                Сounter.Text = "Количество шагов:" + Counters;
+
+            }
+            if(Empty.X==180 && Empty.Y==180)
+            {
+                Check();
+            }
+            void Check()
+            {
+                int count = 0, index;
+                foreach (Button btn1 in panel1.Controls)
+                {
+                    index = (btn1.Location.Y / 90) * 3 + btn1.Location.X / 90;
+                    if (image[index] == btn.Image)
+                    {
+                        count++;
+                    }
+                    if (count == 8)
+                    {
+                        MessageBox.Show("You win!");
+                    }
+                }
+            }
+
+
+        }
+
+        private void Start()
+        {
+            label1.Visible = false;
+            panel1.Visible = true;
+            pictureBox1.Visible = true;
+            Сounter.Visible = true;
+            Counters = 0;
+            Сounter.Text = "Количество шагов:" + Counters;
+            foreach (Button b in panel1.Controls)
+            {
+                b.Enabled = true;
+                try
+                {
+                    Image orginal = Properties.Resources.Image;
+
+                    Crop(orginal, 270, 270);
+
+                    AddImage(image);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Application.Exit();
+                }
+            }
+            StartGame.Visible = false;
+            Restart.Visible = true;
+
+
+        }
+
+        private void StartGame_Click_1(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            Counters = 0;
+            Сounter.Text = "Количество шагов:" + Counters;
+            foreach (Button b in panel1.Controls)
+            {
+                b.Enabled = true;
+
+                Image orginal = Properties.Resources.Image;
+
+                Crop(orginal, 270, 270);
+
+                AddImage(image);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Restart.Visible = false;
+            panel1.Visible = false;
+            pictureBox1.Visible = false;
+            Сounter.Visible = false;
+        }
+    }
+}
